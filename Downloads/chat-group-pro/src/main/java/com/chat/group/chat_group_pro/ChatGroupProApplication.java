b3311b0b19b07e2e454cfc9743e0a351410ef445
kossi-cyber <kosjean0@gmail.com>
2025-12-05 11:13:16 +0100

    Upgrade: bump Java to 21; add configuration processor

---
+++ Downloads/chat-group-pro/src/main/java/com/chat/group/chat_group_pro/ChatGroupProApplication.java
@@ -0,0 +1,13 @@
+package com.chat.group.chat_group_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGroupProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGroupProApplication.class, args);
+	}
+
+}
